83fe81800071a53378507ce4cd061f4598da2be1
Ynnyannah <ynnyannah02@gmail.com>
2024-09-16 15:33:00 +0800

    added layout.ts & tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "tailwindcss";
+
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+
+      extend: {
+            fontFamily: {
+              sans: ["Manrope", ...defaultTheme.fontFamily.sans],
+            },
+      },
+
+  plugins: []
+         } as Config;
